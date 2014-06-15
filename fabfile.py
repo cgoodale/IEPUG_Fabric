95f6d6c0f410c3d7c2f9d395b050692d756d6d48
Cameron Goodale <goodale@apache.org>
2014-06-15 15:18:03 -0700

    Add example of using the local command from Fabric

--- fabfile.py
+++ fabfile.py
@@ -11,3 +11,14 @@ def hostname():
 
 def hello(name="Jack"):
     print "Hello, %s!" % (name, )
+
+
+
+"""Use the local api now"""
+
+from fabric.api import local
+
+def local_fabric():
+    local("mkdir /tmp/fabric")
+    local("cd /tmp/fabric && git clone https://github.com/cgoodale/IEPUG_Fabric.git")
+    local("cd /tmp/fabric/IEPUG_Fabric && ls -alhtr")
