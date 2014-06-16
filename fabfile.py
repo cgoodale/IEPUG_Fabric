b13ca50292e89c3d81ed835e5eaa69a2c2516250
Cameron Goodale <goodale@apache.org>
2014-06-15 17:44:16 -0700

    Add demo that shows halted execution

--- fabfile.py
+++ fabfile.py
@@ -22,3 +22,10 @@ def clone_demo(root_dir='/tmp/fabric'):
     local("mkdir %s" % root_dir)
     local("cd %s && git clone https://github.com/cgoodale/IEPUG_Fabric.git" % root_dir)
     local("cd %s/IEPUG_Fabric && ls -alhtr" % root_dir)
+
+def check_demo(root_dir='/tmp/fabric'):
+    local("cd %s/IEPUG_Fabric && wc fabfile.py" % root_dir)
+    local("cd %s/IEPUG_Fabric && vi fabfile.py" % root_dir)
+    local("cd %s/IEPUG_Fabric && git status" % root_dir)
+
+
