8fad147f4bf84051f93522324b2f26eca45db532
Cameron Goodale <goodale@apache.org>
2014-06-15 19:34:13 -0700

    Use the with cd() syntax

--- fabfile.py
+++ fabfile.py
@@ -23,10 +23,13 @@ def clone_demo(root_dir='/tmp/fabric'):
     local("cd %s && git clone https://github.com/cgoodale/IEPUG_Fabric.git" % root_dir)
     local("cd %s/IEPUG_Fabric && ls -alhtr" % root_dir)
 
+from fabric.api import cd
+
 def check_demo(root_dir='/tmp/fabric'):
-    local("cd %s/IEPUG_Fabric && wc fabfile.py" % root_dir)
-    local("cd %s/IEPUG_Fabric && vi fabfile.py" % root_dir)
-    local("cd %s/IEPUG_Fabric && git status" % root_dir)
+    with cd("%s/IEPUG_Fabric" % root_dir):
+        local("wc fabfile.py")
+        local("vi fabfile.py")
+        local("git status")
 
 def clean_demo():
     proceed = raw_input("This will delete /tmp/fabric.  Proceed? y/n:  ")
